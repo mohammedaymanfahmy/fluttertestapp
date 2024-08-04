ea863a5cf050dc1158f9639b34650d34b1cd462c
Mohamed-Ayman-Fahmy <141614324+Mohamed-Ayman-Fahmy@users.noreply.github.com>
2024-08-04 03:29:32 -0700

    my second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //this is my change from user 1
     return GetMaterialApp(
       title: 'Flutter Demo',
       translations: MyTranslation(),
